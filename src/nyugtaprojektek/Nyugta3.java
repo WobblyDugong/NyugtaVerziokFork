019597f43563c0388fe68d3e38da1a793b206b6c
WobblyDugong <banhidibotond@gmail.com>
2022-03-18 13:06:54 +0100

    közelavég

--- src/nyugtaprojektek/Nyugta3.java
+++ src/nyugtaprojektek/Nyugta3.java
@@ -23,53 +23,82 @@ public class Nyugta3 {
         System.out.println(csillagok);
     }
     
+    private static int osszegzes(int[] tetel,String HUF){
+        int osszeg = 0;
+        for (int i = 0; i < tetel.length; i++) {
+            osszeg=osszeg+tetel[i];
+        }
+        kiiras("Összesen",osszeg,HUF);
+        return osszeg;
+    }
+    
+    private static int szervizdijSzamolas(int osszesen, String HUF) {
+        int szervizDijMertek = 10;
+        int szervizDij = osszesen / szervizDijMertek;
+        kiiras("Szervízdíj",szervizDij,HUF);
+        System.out.printf("(%d%%)\n", szervizDijMertek);
+        return szervizDij;
+    }
+    
+    private static void rovidVonal(){
+        String rovidVonal = "_______";
+        System.out.print(rovidVonal);
+    }
+    private static void vonalValaszto(){
+        String rovidVonalValaszto = "      ";
+        System.out.print(rovidVonalValaszto);
+    }
+    private static void ujsor(){
+        System.out.println("");
+    }
+    private static void nyugtaVeg(){
+        ujsor();
+        rovidVonal();
+        vonalValaszto();
+        rovidVonal();
+        ujsor();
+        System.out.print(" Dátum");
+        vonalValaszto();
+        System.out.println("   Név");
+    }
+    
     public static void main(String[] args) {
-        
+        /*visszatérő változók*/
         final String HUF = "Ft";
         int[] tetel = {350,90,1320};
         String szaggatottVonal = "--------------------";
         String duplaVonal = "====================";
         
+        /*kezdő csillagsorok*/
         csillagok("%14s\n","Nyugta 3");
         
+        /*alap tételek*/
         tetelek(tetel[0],tetel[1],tetel[2],HUF);
-
         System.out.println(duplaVonal);
         
-        int osszesen = tetel[0] + tetel[1] + tetel[2];
-        kiiras("Összesen",osszesen,HUF);
-        
+        /*összegzés*/
+        int osszesen = osszegzes(tetel,HUF);
         System.out.println(szaggatottVonal);
         
-        int szervizDijMertek = 10;
-        int szervizDij = osszesen / szervizDijMertek;
-        kiiras("Szervízdíj",szervizDij,HUF);
-        System.out.printf("(%d%%)\n", szervizDijMertek);
-        
+        /*szervizdij*/
+        int szervizDij = szervizdijSzamolas(osszesen,HUF);
         System.out.println(duplaVonal);
         
+        /*fizetendő*/
         int fizetendo = osszesen + szervizDij;
         kiiras("Fizetendő",fizetendo,HUF);
         
+        /*euróban*/
         double euro = fizetendo / 373.0;
         final String eur = "\u20ac";
-
         System.out.printf("%10s%7.2f %s\n","", euro, eur);//
-        
         System.out.println(szaggatottVonal);
         
-        System.out.println("");
-        String rovidVonal = "_______";
-        System.out.print(rovidVonal);
-        String rovidVonalValaszto = "      ";
-        System.out.print(rovidVonalValaszto);
-        System.out.println(rovidVonal);
-        System.out.print(" Dátum");
-        System.out.print(rovidVonalValaszto);
-        System.out.println("   Név");
+        /*Aláírás*/
+        nyugtaVeg();
         
+        /*záró csillagsorok*/
         csillagok("%11s\n","CÉG");
         
     }
-
 }
